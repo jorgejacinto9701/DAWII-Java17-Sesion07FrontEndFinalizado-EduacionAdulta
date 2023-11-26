e6132cbf706b6c4e456b4ba02ab908b01041c878
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2023-11-26 12:52:57 -0500

    export excel

--- src/app/services/docente.service.ts
+++ src/app/services/docente.service.ts
@@ -67,6 +67,30 @@ export class DocenteService {
             data: new Blob([response], {type: 'application/pdf'})
         };
     }));
-}
+  }
+
+
+  generateDocumentExcel(nombre:string,
+    dni:string,
+    estado:number,
+    idUbigeo:number): Observable<any> {
+
+        const params = new HttpParams()
+        .set("nombre", nombre)
+        .set("dni", dni)
+        .set("estado", estado)
+        .set("idUbigeo", idUbigeo);
+
+        let headers = new HttpHeaders();
+        headers.append('Accept', 'application/vnd.ms-excel');
+        let requestOptions: any = { headers: headers, responseType: 'blob' };
+
+        return this.http.post(baseUrlConsulta +"/reporteDocenteExcel?nombre="+nombre+"&dni="+dni+"&estado="+estado+"&idUbigeo="+idUbigeo,'', requestOptions).pipe(map((response)=>{
+        return {
+        filename: 'reporteDocente20232.xlsx',
+        data: new Blob([response], {type: 'application/vnd.ms-excel'})
+        };
+        }));
+    }
 
 }
